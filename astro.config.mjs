637574aa0625fdd3d818d1bfdafa920f6bfc754f
Johnny Noble <github-a3bd@johnny.net.au>
2024-11-16 17:34:58 +1100

    rudimentary ops manuals

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  server: {
+    port: 3001,
+  },
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
 });
