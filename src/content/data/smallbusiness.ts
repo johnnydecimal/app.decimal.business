1c83fa6431067ab5ede8920d49c6a6210cb14540
Johnny Noble <github-a3bd@johnny.net.au>
2024-11-18 14:41:47 +1100

    copy button

--- src/content/data/smallbusiness.ts
+++ src/content/data/smallbusiness.ts
@@ -65,6 +65,28 @@ const smallBusiness: JohnnyDecimalSystem = {
                 },
               },
             },
+            "11.12": {
+              id: "11.12",
+              title: "Licences, permits, & accreditations",
+              description:
+                "Documents that you apply for, or are earned via training, so you can legally provide a service or operate in a particular location.",
+              metadata: {
+                createdDate: "2024-11-18",
+                updatedDate: "2024-11-18",
+              },
+              extensions: {
+                smallBusiness: {
+                  examples:
+                    "Liquor licence; safe food handling certificate; truck or taxi licence; police check; zoning permit; government security clearance; market stall holder permit; working with children check; tradesperson licence; responsible service of alcohol certificate; environmental permit; any other legally-required core qualification.",
+                  moreInfo:
+                    "This ID is for storing copies of any licence, permit, or accreditation that your business cannot trade without. First, you registered your business. Now you need the legal proof that you can provide a product or service.\n\nThis ‘proof’ will vary considerably depending on what you do. If you run a bar, you’ll need a liquor licence. If you’re a healthcare provider in private practice, you must have a degree, as well as any other required accreditations.\n\nYou might be thinking, “hey Johnny.Decimal, you’ve always said not to have copies of things in different places”. Absolutely – for documents that change, like your budget spreadsheet. You should never have multiple copies of things like that because someone will inevitably update the wrong version.\n\nHowever, licences, permits, and accreditations are static documents. Probably a PDF or a piece of paper. It’s okay to put copies of them where it’s helpful and makes sense to do so. They’re not going to change unless they are renewed.\n\nThis is an important ID to keep up-to-date in your JDex and to set reminders for to ensure nothing expires.",
+                  alsoSee:
+                    "Manage ongoing training or education in [[11.50]], but store a copy of the proof of completion here if it’s essential to your ability to trade.",
+                  rationale:
+                    "If an inspector turns up at your door, having copies of these core documents in one place will be very handy. They’re also highly visible at the top of your system so they’re not lost or forgotten.",
+                },
+              },
+            },
           },
         },
       },
