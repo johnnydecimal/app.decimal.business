aec1ebf94b47a0bbedcd72aed105f275bffe9181
Johnny Noble <github-a3bd@johnny.net.au>
2024-11-19 16:40:06 +1100

    add clerk, configure for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import clerk from "@clerk/astro";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3001,
   },
+
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [clerk(), mdx(), sitemap()],
+  output: "server",
+  adapter: vercel(),
 });
