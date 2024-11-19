f6e7131c38296711a7a970a001050258db9774b6
Johnny Noble <github-a3bd@johnny.net.au>
2024-11-19 15:25:53 +1100

    Clerk auth basically working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import clerk from "@clerk/astro";
 import mdx from "@astrojs/mdx";
-
+import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -9,6 +10,9 @@ export default defineConfig({
   server: {
     port: 3001,
   },
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+
+  site: "https://app.decimal.business",
+  integrations: [clerk(), mdx(), sitemap()],
+  output: "server",
+  adapter: netlify(),
 });
