68489cbc13097ec53f7554137222d2c924005cbd
Johnny Noble <github-a3bd@johnny.net.au>
2024-11-21 09:10:14 +1100

    wip

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,13 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
 const isProtectedRoute = createRouteMatcher(["/11(.*)"]);
 
-export const onRequest = clerkMiddleware((auth, context) => {
-  const { redirectToSignIn, userId } = auth();
+export const onRequest = clerkMiddleware(
+  (auth, context) => {
+    const { redirectToSignIn, userId } = auth();
 
-  if (!userId && isProtectedRoute(context.request)) {
-    return redirectToSignIn();
-  }
-});
+    if (!userId && isProtectedRoute(context.request)) {
+      return redirectToSignIn();
+    }
+  },
+  { debug: true }
+);
