a37b6cdf474b1d47a9b47240f1521d60a30e7d5a
Johnny Noble <github-a3bd@johnny.net.au>
2024-11-23 11:40:49 +1100

    about to futz with TS settings

---
+++ src/scripts/generateIds.js
@@ -0,0 +1,19 @@
+// External
+import { writeFile } from "fs/promises";
+// Internal
+import generateIdsWithACContext from "../helpers/generateIdsWithACContext.ts";
+// Data
+import sb from "../content/data/smallbusiness.ts";
+
+(async () => {
+  const idsWithACContext = await generateIdsWithACContext(sb);
+
+  await writeFile(
+    "./src/data/idsWithACContext.json",
+    JSON.stringify(idsWithACContext, null, 2),
+    "utf-8"
+  );
+  console.log(
+    "Generated IDs with AC context and saved to idsWithACContext.json"
+  );
+})();
