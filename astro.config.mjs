5b3c87f831e403872d922922fc6f5d604dd73115
Johnny Noble <github-a3bd@johnny.net.au>
2025-01-19 12:51:00 +1100

    Pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
-import sitemap from "@astrojs/sitemap";
-
-import netlify from "@astrojs/netlify";
 import clerk from "@clerk/astro";
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
+  build: {
+    format: "file",
+  },
+  integrations: [clerk(), mdx(), pagefind()],
+  output: "server",
   server: {
     port: 3011,
   },
-
   site: "https://app.johnnydecimal.com",
-  integrations: [mdx(), sitemap(), clerk()],
-  output: "server",
-  adapter: netlify(),
   vite: {
     css: {
       preprocessorOptions: {
