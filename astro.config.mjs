b69c5f99f052990290f41b73630e44d86667b9e8
Johnny Noble <github-a3bd@johnny.net.au>
2025-01-19 13:06:36 +1100

    Revert "Pagefind"
    This reverts commit 5b3c87f831e403872d922922fc6f5d604dd73115.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import clerk from "@clerk/astro";
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
 import netlify from "@astrojs/netlify";
-import pagefind from "astro-pagefind";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
-  build: {
-    format: "file",
-  },
-  integrations: [clerk(), mdx(), pagefind()],
-  output: "server",
   server: {
     port: 3011,
   },
+
   site: "https://app.johnnydecimal.com",
+  integrations: [mdx(), sitemap(), clerk()],
+  output: "server",
+  adapter: netlify(),
   vite: {
     css: {
       preprocessorOptions: {
