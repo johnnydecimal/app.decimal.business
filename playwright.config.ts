2ce588ef577c5e3ec6175b9789dfc9fa33f6bf94
Johnny Noble <github-a3bd@johnny.net.au>
2025-01-31 17:00:44 +1100

    progress

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,26 +1,35 @@
-// import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 
-// export default defineConfig({
-//   testDir: "./tests",
-//   fullyParallel: true,
-//   retries: 1,
-//   reporter: [["html", { open: "never" }]],
-//   use: {
-//     baseURL: "http://localhost:3000",
-//     trace: "on-first-retry",
-//   },
-//   projects: [
-//     {
-//       name: "Desktop Chrome",
-//       use: { ...devices["Desktop Chrome"] },
-//     },
-//     {
-//       name: "Desktop Firefox",
-//       use: { ...devices["Desktop Firefox"] },
-//     },
-//     {
-//       name: "Desktop Safari",
-//       use: { ...devices["Desktop Safari"] },
-//     },
-//   ],
-// });
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  retries: 1,
+  reporter: [["html", { open: "never" }]],
+  use: {
+    baseURL: "http://localhost:3012",
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "Desktop Chrome",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1280, height: 720 },
+      },
+    },
+    {
+      name: "Desktop Firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        viewport: { width: 1280, height: 720 },
+      },
+    },
+    {
+      name: "Desktop Safari",
+      use: {
+        ...devices["Desktop Safari"],
+        viewport: { width: 1280, height: 720 },
+      },
+    },
+  ],
+});
