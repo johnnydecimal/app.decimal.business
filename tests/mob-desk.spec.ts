932c312c7c48a10cf2535e877391359583266ab7
Johnny Noble <github-a3bd@johnny.net.au>
2025-01-31 17:26:13 +1100

    tests barely work but they do - new LayoutNoAuth component

---
+++ tests/mob-desk.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+import { devices } from "@playwright/test";
+
+test.describe("Visual Tests", () => {
+  test("Desktop snapshot", async ({ page }) => {
+    await page.setViewportSize({ width: 1280, height: 720 });
+    await page.goto("/test-area2");
+    expect(await page.screenshot()).toMatchSnapshot("desktop.png");
+  });
+
+  test("Mobile snapshot", async ({ page }) => {
+    await page.setViewportSize({ width: 375, height: 812 });
+    await page.goto("/test-area2");
+    expect(await page.screenshot()).toMatchSnapshot("mobile.png");
+  });
+});
