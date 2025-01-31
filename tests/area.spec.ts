ffe0bf3858ba85f3b1a5bd39fa7874011d4f53ff
Johnny Noble <github-a3bd@johnny.net.au>
2025-01-31 17:33:16 +1100

    tests now work from /tests/...

--- tests/area.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test.describe("Area Component Visual Regression Tests", () => {
-  test("Area Component", async ({ page }) => {
-    await page.goto("/test-area");
-    expect(await page.screenshot()).toMatchSnapshot("area-component.png");
-  });
-});
