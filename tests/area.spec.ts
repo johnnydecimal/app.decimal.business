929dd92ad5ff7daab94ae46fdd82019203dce228
Johnny Noble <github-a3bd@johnny.net.au>
2025-02-01 13:25:18 +1100

    tests now have fullPage: true

--- tests/area.spec.ts
+++ tests/area.spec.ts
@@ -2,9 +2,9 @@ import { test, expect } from "@playwright/test";
 
 test("Area 10-19", async ({ page }) => {
   await page.goto("/test/10-19");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
 test("Area 20-29", async ({ page }) => {
   await page.goto("/test/20-29");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
--- tests/system.spec.ts
+++ tests/system.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("System", async ({ page }) => {
   await page.goto("/test");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
