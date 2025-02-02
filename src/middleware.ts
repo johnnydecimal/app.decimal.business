24e47ec308a9475255666d9390c93322a4d8f74c
Johnny Noble <github-a3bd@johnny.net.au>
2025-02-02 15:38:19 +1100

    add bare-bones middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
+// https://clerk.com/docs/references/astro/clerk-middleware
+import { clerkMiddleware } from "@clerk/astro/server";
 
-const isProtectedRoute = createRouteMatcher(["/jdex(.*)"]);
+export const onRequest = clerkMiddleware();
 
-export const onRequest = clerkMiddleware((auth, context) => {
-  const { redirectToSignIn, userId } = auth();
+/* ----------------------------------------------------------------------- */
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-  // console.log("middleware: userId", userId);
+// const isProtectedRoute = createRouteMatcher(["/jdex(.*)"]);
 
-  if (!userId && isProtectedRoute(context.request)) {
-    // Add custom logic to run before redirecting
+// export const onRequest = clerkMiddleware((auth, context) => {
+// const { redirectToSignIn, userId } = auth();
 
-    return redirectToSignIn();
-  }
-});
+// // console.log("middleware: userId", userId);
+
+// if (!userId && isProtectedRoute(context.request)) {
+//   // Add custom logic to run before redirecting
+
+//   return redirectToSignIn();
+// }
+// });
