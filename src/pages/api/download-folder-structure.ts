5cefe97ecb1b24ebe7fd5f6be433ea43b905b506
Johnny Noble <github-a3bd@johnny.net.au>
2025-02-06 09:38:42 +1100

    rudimentary emoji in download folders

--- src/pages/api/download-folder-structure.ts
+++ src/pages/api/download-folder-structure.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
+import { createClerkClient } from "@clerk/astro/server";
 import JSZip from "jszip";
-import { clerkClient } from "@clerk/astro/server";
 
 import system, {
   getAllByType,
@@ -9,15 +9,18 @@ import system, {
   type IdEntry,
 } from "@data/smallBusinessFlat.ts";
 
-// export const GET: APIRoute = async ({ request }) => {
 export const GET: APIRoute = async (context) => {
-  const zip = new JSZip();
-  console.log(context.locals.auth());
-  const userId = context.locals.auth().userId;
+  const clerkClient = createClerkClient({
+    secretKey: import.meta.env.CLERK_SECRET_KEY,
+  });
 
-  const clerk = clerkClient(context);
-  const user = await clerk.users.getUser(userId!);
-  console.log("user", user);
+  const userId = context.locals.auth().userId;
+  if (!userId) throw new Error("This is impossible, route /api is protected.");
+
+  const user = await clerkClient.users.getUser(userId);
+  const useEmoji = user.publicMetadata.useEmoji || false;
+
+  const zip = new JSZip();
 
   const ids = getAllByType(system, "id") as IdEntry[];
   ids.forEach((id) => {
@@ -27,19 +30,29 @@ export const GET: APIRoute = async (context) => {
     const areaNumber = categoryEntry.parentNumber;
     const areaEntry = system[areaNumber] as AreaEntry;
     const areaTitle = areaEntry.title;
-    zip.folder(
-      `${areaNumber} ${areaTitle}/${categoryNumber} ${categoryTitle}/${id.number} ${id.title}`
-    );
+    let idEmoji, categoryEmoji, areaEmoji;
+    if (useEmoji) {
+      idEmoji = id.emoji ? " " + id.emoji : "";
+      categoryEmoji = categoryEntry.emoji ? " " + categoryEntry.emoji : "";
+      areaEmoji = areaEntry.emoji ? " " + areaEntry.emoji : "";
+      zip.folder(
+        `${areaNumber} ${areaTitle}${areaEmoji}/${categoryNumber} ${categoryTitle}${categoryEmoji}/${id.number} ${id.title}${idEmoji}`
+      );
+    } else {
+      zip.folder(
+        `${areaNumber} ${areaTitle}/${categoryNumber} ${categoryTitle}/${id.number} ${id.title}`
+      );
+    }
   });
 
   const content = await zip.generateAsync({ type: "nodebuffer" });
 
-  return new Response(JSON.stringify({ message: "This was a GET!" }));
-  // return new Response(content, {
-  //   headers: {
-  //     "Content-Type": "application/zip",
-  //     "Content-Disposition":
-  //       'attachment; filename="Johnny.Decimal Small Business System - folder structure.zip"',
-  //   },
-  // });
+  // return new Response(JSON.stringify({ message: "This was a GET!" }));
+  return new Response(content, {
+    headers: {
+      "Content-Type": "application/zip",
+      "Content-Disposition":
+        'attachment; filename="Johnny.Decimal Small Business System - folder structure.zip"',
+    },
+  });
 };
