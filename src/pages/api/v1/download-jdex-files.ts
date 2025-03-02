8e5ae6e6097f059efec012d4c466a1a63eef0510
Johnny Noble <github-a3bd@johnny.net.au>
2025-03-02 15:44:51 +1100

    JDex downloader is now an Obsidian vault

--- src/pages/api/v1/download-jdex-files.ts
+++ src/pages/api/v1/download-jdex-files.ts
@@ -8,6 +8,7 @@ import system, {
   type CategoryEntry,
   type IdEntry,
 } from "@data/smallBusinessFlat.ts";
+import { z } from "astro:content";
 
 /**
  * API endpoint to download Johnny.Decimal files as a ZIP archive
@@ -122,6 +123,240 @@ export const GET: APIRoute = async (context) => {
       zip.file(fileName, markdownContent);
     }
   });
+
+  // #region Make it an Obsidian vault
+  zip.folder(".obsidian");
+  zip.file(".obsidian/app.json", "{}");
+  // appearance.json
+  zip.file(
+    ".obsidian/appearance.json",
+    `{
+  "accentColor": "#f04c00",
+  "showRibbon": false
+}`
+  );
+  // core-plugins.json
+  zip.file(
+    ".obsidian/core-plugins.json",
+    `{
+  "file-explorer": true,
+  "global-search": true,
+  "switcher": true,
+  "graph": true,
+  "backlink": true,
+  "canvas": true,
+  "outgoing-link": true,
+  "tag-pane": true,
+  "properties": false,
+  "page-preview": true,
+  "daily-notes": true,
+  "templates": true,
+  "note-composer": true,
+  "command-palette": true,
+  "slash-command": false,
+  "editor-status": true,
+  "bookmarks": true,
+  "markdown-importer": false,
+  "zk-prefixer": false,
+  "random-note": false,
+  "outline": true,
+  "word-count": true,
+  "slides": false,
+  "audio-recorder": false,
+  "workspaces": false,
+  "file-recovery": true,
+  "publish": false,
+  "sync": true,
+  "webviewer": false
+}`
+  );
+  // workspace.json
+  zip.file(
+    ".obsidian/workspace.json",
+    `{
+  "main": {
+    "id": "94d7e370b5189eca",
+    "type": "split",
+    "children": [
+      {
+        "id": "10dbed942e78f3e8",
+        "type": "tabs",
+        "children": [
+          {
+            "id": "b632958dbee9b3c6",
+            "type": "leaf",
+            "state": {
+              "type": "markdown",
+              "state": {
+                "file": "Welcome to your JDex in Obsidian.md",
+                "mode": "source",
+                "source": false
+              },
+              "icon": "lucide-file",
+              "title": "Welcome to your JDex in Obsidian"
+            }
+          }
+        ]
+      }
+    ],
+    "direction": "vertical"
+  },
+  "left": {
+    "id": "b09c98036e7d885a",
+    "type": "split",
+    "children": [
+      {
+        "id": "ab58a0d54fdd48a0",
+        "type": "tabs",
+        "children": [
+          {
+            "id": "6b087cb5180d616e",
+            "type": "leaf",
+            "state": {
+              "type": "file-explorer",
+              "state": {
+                "sortOrder": "alphabetical",
+                "autoReveal": false
+              },
+              "icon": "lucide-folder-closed",
+              "title": "File explorer"
+            }
+          },
+          {
+            "id": "c61f4bcbe1a26584",
+            "type": "leaf",
+            "state": {
+              "type": "search",
+              "state": {
+                "query": "",
+                "matchingCase": false,
+                "explainSearch": false,
+                "collapseAll": false,
+                "extraContext": false,
+                "sortOrder": "alphabetical"
+              },
+              "icon": "lucide-search",
+              "title": "Search"
+            }
+          },
+          {
+            "id": "f81eaa92f4d322b3",
+            "type": "leaf",
+            "state": {
+              "type": "bookmarks",
+              "state": {},
+              "icon": "lucide-bookmark",
+              "title": "Bookmarks"
+            }
+          }
+        ]
+      }
+    ],
+    "direction": "horizontal",
+    "width": 300
+  },
+  "right": {
+    "id": "bd00adf8ff7f135d",
+    "type": "split",
+    "children": [
+      {
+        "id": "9001c30eb379c1f6",
+        "type": "tabs",
+        "children": [
+          {
+            "id": "d5aca17d20f4785c",
+            "type": "leaf",
+            "state": {
+              "type": "backlink",
+              "state": {
+                "file": "Welcome to your JDex in Obsidian.md",
+                "collapseAll": false,
+                "extraContext": false,
+                "sortOrder": "alphabetical",
+                "showSearch": false,
+                "searchQuery": "",
+                "backlinkCollapsed": false,
+                "unlinkedCollapsed": true
+              },
+              "icon": "links-coming-in",
+              "title": "Backlinks for Welcome to your JDex in Obsidian"
+            }
+          },
+          {
+            "id": "10343962523ed972",
+            "type": "leaf",
+            "state": {
+              "type": "outgoing-link",
+              "state": {
+                "file": "Welcome to your JDex in Obsidian.md",
+                "linksCollapsed": false,
+                "unlinkedCollapsed": true
+              },
+              "icon": "links-going-out",
+              "title": "Outgoing links from Welcome to your JDex in Obsidian"
+            }
+          },
+          {
+            "id": "e8ce98ac70890dbe",
+            "type": "leaf",
+            "state": {
+              "type": "tag",
+              "state": {
+                "sortOrder": "frequency",
+                "useHierarchy": true,
+                "showSearch": false,
+                "searchQuery": ""
+              },
+              "icon": "lucide-tags",
+              "title": "Tags"
+            }
+          },
+          {
+            "id": "57d7b0d25baf607a",
+            "type": "leaf",
+            "state": {
+              "type": "outline",
+              "state": {
+                "file": "Welcome to your JDex in Obsidian.md",
+                "followCursor": false,
+                "showSearch": false,
+                "searchQuery": ""
+              },
+              "icon": "lucide-list",
+              "title": "Outline of Welcome to your JDex in Obsidian"
+            }
+          }
+        ]
+      }
+    ],
+    "direction": "horizontal",
+    "width": 300,
+    "collapsed": true
+  },
+  "left-ribbon": {
+    "hiddenItems": {
+      "switcher:Open quick switcher": false,
+      "graph:Open graph view": false,
+      "canvas:Create new canvas": false,
+      "daily-notes:Open today's note": false,
+      "templates:Insert template": false,
+      "command-palette:Open command palette": false
+    }
+  },
+  "active": "b632958dbee9b3c6",
+  "lastOpenFiles": []
+}`
+  );
+  // Welcome file
+  zip.file(
+    "Welcome to your JDex in Obsidian.md",
+    `This system is under construction.
+    
+When it's complete, this download will contain a copy of each of the IDs from the system.
+
+This becomes your local [JDex](https://jdcm.al/11.05).`
+  );
+  // #endregion
 
   const content = await zip.generateAsync({ type: "nodebuffer" });
 
