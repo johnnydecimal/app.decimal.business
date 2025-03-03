0c81cb257a7bc8323eda588ae9ae7e706c559d61
Johnny Noble <github-a3bd@johnny.net.au>
2025-03-04 10:01:45 +1100

    update tests

--- src/utils/slugToId.ts
+++ src/utils/slugToId.ts
@@ -1,6 +1,12 @@
 function slugToId(slug: string | undefined): string {
   if (slug === undefined || slug === "index.html") {
     return "J82";
+  } else if (slug === "favicon.svg") {
+    // Firefox preloading it?
+    // BUG probably needs fixing
+    // If you remove this as a return path, and load the home page in Firefox,
+    // you'll see an error.
+    return "J82";
   } else {
     return slug;
   }
